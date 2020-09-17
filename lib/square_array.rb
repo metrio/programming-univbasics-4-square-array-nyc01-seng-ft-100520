23008cf23b4a3537d6063e8578129ac7d2db321b
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-17 15:13:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,3 +9,14 @@ numbers.length.times do |index|
 end
 new_numbers
 end
+
+#def square_array(numbers)
+counter = 0
+new_numbers = []
+  
+  while numbers[counter] do
+  new_numbers << numbers[counter] ** 2
+  counter += 1 
+  end
+new_numbers
+end
